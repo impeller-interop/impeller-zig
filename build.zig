f226104fe1829dada03fd7e5a4124c371ac69650
KercyDing <dkx215417@gmail.com>
2026-05-20 05:06:12 +0800

    chore(sdk): use impeller-sdk dependency

--- build.zig
+++ build.zig
@@ -9,7 +9,6 @@ const ImpellerSdk = struct {
     header: std.Build.LazyPath,
     include_path: std.Build.LazyPath,
     lib_path: std.Build.LazyPath,
-    lib_path_string: []const u8,
     library: std.Build.LazyPath,
     import_library: ?std.Build.LazyPath,
 };
@@ -22,18 +21,43 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     };
 
-    const sdk = impellerSdk(b, target.result) orelse {
-        addPendingModule(b, options);
-        return;
-    };
-    addSdkPaths(b, sdk);
-    const mod = addModule(b, options, sdk);
+    const sdk = getSdk(b, options);
+    exposeSdk(b, sdk);
 
-    addLibraryArtifact(b, mod);
+    const mod = addModule(b, options, sdk);
+    addArtifact(b, mod);
     addTests(b, options, sdk, mod);
 }
 
-fn addLibraryArtifact(b: *std.Build, mod: *std.Build.Module) void {
+fn getSdk(b: *std.Build, options: BuildOptions) ImpellerSdk {
+    const dep = b.dependency("impeller_sdk", .{
+        .target = options.target,
+        .optimize = options.optimize,
+    });
+
+    return .{
+        .header = dep.namedLazyPath("impeller_header"),
+        .include_path = dep.namedLazyPath("impeller_include"),
+        .lib_path = dep.namedLazyPath("impeller_lib_dir"),
+        .library = dep.namedLazyPath("impeller_library"),
+        .import_library = if (options.target.result.os.tag == .windows)
+            dep.namedLazyPath("impeller_import_library")
+        else
+            null,
+    };
+}
+
+fn exposeSdk(b: *std.Build, sdk: ImpellerSdk) void {
+    b.addNamedLazyPath("impeller_header", sdk.header);
+    b.addNamedLazyPath("impeller_include", sdk.include_path);
+    b.addNamedLazyPath("impeller_lib_dir", sdk.lib_path);
+    b.addNamedLazyPath("impeller_library", sdk.library);
+    if (sdk.import_library) |import_library| {
+        b.addNamedLazyPath("impeller_import_library", import_library);
+    }
+}
+
+fn addArtifact(b: *std.Build, mod: *std.Build.Module) void {
     const lib = b.addLibrary(.{
         .name = "impeller",
         .root_module = mod,
@@ -41,22 +65,8 @@ fn addLibraryArtifact(b: *std.Build, mod: *std.Build.Module) void {
     b.installArtifact(lib);
 }
 
-fn addSdkPaths(b: *std.Build, sdk: ImpellerSdk) void {
-    b.addNamedLazyPath("impeller_include", sdk.include_path);
-    b.addNamedLazyPath("impeller_lib_dir", sdk.lib_path);
-    b.addNamedLazyPath("impeller_library", sdk.library);
-}
-
-fn addPendingModule(b: *std.Build, options: BuildOptions) void {
-    _ = b.addModule("impeller", .{
-        .root_source_file = b.path("src/impeller.zig"),
-        .target = options.target,
-        .optimize = options.optimize,
-    });
-}
-
 fn addModule(b: *std.Build, options: BuildOptions, sdk: ImpellerSdk) *std.Build.Module {
-    const impeller_c = addImpellerBindings(b, options, sdk);
+    const impeller_c = addRawModule(b, options, sdk);
     const mod = b.addModule("impeller", .{
         .root_source_file = b.path("src/impeller.zig"),
         .target = options.target,
@@ -65,8 +75,8 @@ fn addModule(b: *std.Build, options: BuildOptions, sdk: ImpellerSdk) *std.Build.
             .{ .name = "impeller_c", .module = impeller_c },
         },
     });
-    configureImpeller(mod, sdk);
-    linkImpeller(mod, sdk, options.target.result);
+
+    linkSdk(mod, sdk, options.target.result);
     return mod;
 }
 
@@ -79,7 +89,7 @@ fn addTests(b: *std.Build, options: BuildOptions, sdk: ImpellerSdk, mod: *std.Bu
             .{ .name = "impeller", .module = mod },
         },
     });
-    configureImpeller(test_mod, sdk);
+    linkSdk(test_mod, sdk, options.target.result);
 
     const tests = b.addTest(.{
         .root_module = test_mod,
@@ -88,13 +98,13 @@ fn addTests(b: *std.Build, options: BuildOptions, sdk: ImpellerSdk, mod: *std.Bu
     });
 
     const run_tests = b.addRunArtifact(tests);
-    configureImpellerRuntime(run_tests, sdk);
+    addRuntimePath(run_tests, sdk);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_tests.step);
 }
 
-fn addImpellerBindings(b: *std.Build, options: BuildOptions, sdk: ImpellerSdk) *std.Build.Module {
+fn addRawModule(b: *std.Build, options: BuildOptions, sdk: ImpellerSdk) *std.Build.Module {
     const translate = b.addTranslateC(.{
         .root_source_file = sdk.header,
         .target = options.target,
@@ -104,89 +114,19 @@ fn addImpellerBindings(b: *std.Build, options: BuildOptions, sdk: ImpellerSdk) *
     return translate.createModule();
 }
 
-fn configureImpeller(module: *std.Build.Module, sdk: ImpellerSdk) void {
-    module.addIncludePath(sdk.include_path);
-    module.addRPath(sdk.lib_path);
-}
-
-fn linkImpeller(module: *std.Build.Module, sdk: ImpellerSdk, target: std.Target) void {
+fn linkSdk(mod: *std.Build.Module, sdk: ImpellerSdk, target: std.Target) void {
+    mod.addIncludePath(sdk.include_path);
+    mod.addRPath(sdk.lib_path);
     if (target.os.tag == .windows) {
-        module.addObjectFile(sdk.import_library.?);
+        mod.addObjectFile(sdk.import_library.?);
     } else {
-        module.addObjectFile(sdk.library);
+        mod.addObjectFile(sdk.library);
     }
 }
 
-fn configureImpellerRuntime(run: *std.Build.Step.Run, sdk: ImpellerSdk) void {
-    run.setEnvironmentVariable("DYLD_LIBRARY_PATH", sdk.lib_path_string);
-    run.setEnvironmentVariable("LD_LIBRARY_PATH", sdk.lib_path_string);
-    run.addPathDir(sdk.lib_path_string);
-}
-
-fn impellerSdk(b: *std.Build, target: std.Target) ?ImpellerSdk {
-    const dep_name = sdkDepName(target) orelse @panic("unsupported Impeller SDK target");
-    const dep = b.lazyDependency(dep_name, .{}) orelse return null;
-    const include_path = dep.path("include");
-    const os_dir = libOsDir(target);
-    const arch_dir = libArchDir(target);
-    const lib_path = dep.builder.fmt("lib/{s}/{s}", .{ os_dir, arch_dir });
-    return .{
-        .header = dep.path("include/impeller.h"),
-        .include_path = include_path,
-        .lib_path = dep.path(lib_path),
-        .lib_path_string = dep.builder.pathFromRoot(lib_path),
-        .library = dep.path(dep.builder.fmt("{s}/{s}", .{ lib_path, libName(target) })),
-        .import_library = if (target.os.tag == .windows)
-            dep.path(dep.builder.fmt("{s}/{s}", .{ lib_path, importLibName }))
-        else
-            null,
-    };
-}
-
-fn sdkDepName(target: std.Target) ?[]const u8 {
-    return switch (target.os.tag) {
-        .macos => switch (target.cpu.arch) {
-            .aarch64 => "impeller_sdk_macos_arm64",
-            .x86_64 => "impeller_sdk_macos_x64",
-            else => null,
-        },
-        .linux => switch (target.cpu.arch) {
-            .aarch64 => "impeller_sdk_linux_arm64",
-            .x86_64 => "impeller_sdk_linux_x64",
-            else => null,
-        },
-        .windows => switch (target.cpu.arch) {
-            .aarch64 => "impeller_sdk_windows_arm64",
-            .x86_64 => "impeller_sdk_windows_x64",
-            else => null,
-        },
-        else => null,
-    };
-}
-
-fn libName(target: std.Target) []const u8 {
-    return switch (target.os.tag) {
-        .macos => "libimpeller.dylib",
-        .windows => "impeller.dll",
-        else => "libimpeller.so",
-    };
-}
-
-const importLibName = "impeller.dll.lib";
-
-fn libOsDir(target: std.Target) []const u8 {
-    return switch (target.os.tag) {
-        .macos => "macos",
-        .linux => "linux",
-        .windows => "windows",
-        else => @panic("unsupported Impeller SDK target"),
-    };
-}
-
-fn libArchDir(target: std.Target) []const u8 {
-    return switch (target.cpu.arch) {
-        .aarch64 => "arm64",
-        .x86_64 => "x64",
-        else => @panic("unsupported Impeller SDK target architecture"),
-    };
+fn addRuntimePath(run: *std.Build.Step.Run, sdk: ImpellerSdk) void {
+    const lib_path = sdk.lib_path.getPath2(run.step.owner, &run.step);
+    run.setEnvironmentVariable("DYLD_LIBRARY_PATH", lib_path);
+    run.setEnvironmentVariable("LD_LIBRARY_PATH", lib_path);
+    run.addPathDir(lib_path);
 }
